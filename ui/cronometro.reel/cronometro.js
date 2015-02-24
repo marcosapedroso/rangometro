47fe292827bb3183118bb532c9c53c584d6c2057
Marcos Pedroso <marcosapedroso@yahoo.com.br>
2015-02-24 18:23:18 +0000

    Add component cronometro

---
+++ ui/cronometro.reel/cronometro.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/cronometro.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Cronometro
+ * @extends Component
+ */
+exports.Cronometro = Component.specialize(/** @lends Cronometro# */ {
+    constructor: {
+        value: function Cronometro() {
+            this.super();
+        }
+    }
+});
